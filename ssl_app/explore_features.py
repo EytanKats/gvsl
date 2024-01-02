bc90f162321ffec62a316e7c9047b55ebc716c93
EytanKats <eytan.kats@gmail.com>
2024-01-02 11:29:43 +0100

    add features exploration for FPN architecture

--- ssl_app/explore_features.py
+++ ssl_app/explore_features.py
@@ -1,14 +1,18 @@
 import os
-
-import torch
 import numpy as np
-
+import matplotlib.pyplot as plt
 from monai.data import Dataset, DataLoader
 
+import torch
+from torch.nn import functional
+
+from vox2vec.nn import FPN3d
 from exploration.gvsl import GVSL
 from ssl_app.core.plots import plot_tsne
 from ssl_app.core.unet3d_gvsl import UNet3D_GVSL
+from ssl_app.core.coupled_convex import coupled_convex
 from ssl_app.blocks.data_loaders import read_json_data_file, get_val_transform
+
 
 
 OUTPUT_DIR = '/mnt/share/experiments/label/gvsl/tsne/'
@@ -16,6 +20,7 @@ OUTPUT_DIR = '/mnt/share/experiments/label/gvsl/tsne/'
 pretrained_weights = [
     # '',
     # '/mnt/share/experiments/label/gvsl/original/GVSL_epoch_1000.pth',
+    # '/mnt/share/experiments/label/gvsl/nako_1000_pretraining/GVSL_epoch_1000.pth',
     # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_restoration/0/checkpoint/ckpt-49.pth',
     # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_registration/0/checkpoint/ckpt-0.pth',
     # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_registration/0/checkpoint/ckpt-49.pth',
@@ -23,12 +28,14 @@ pretrained_weights = [
     # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_registration_restoration/0/checkpoint/ckpt-49.pth',
     # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_registration_constrained/0/checkpoint/ckpt-49.pth',
     # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_coupled_convex/0/checkpoint/ckpt-49.pth',
-    '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_global_local/0/checkpoint/ckpt-44.pth'
+    # '/mnt/share/experiments/label/gvsl/sc_nako1000_pretraining_global_local/0/checkpoint/ckpt-49.pth',
+    '/mnt/share/experiments/label/vox2vec/ssl_models/nako1000_contrastive_restoration_fixed_epoch=499-step=50000.ckpt'
 ]
 
 plot_names = [
     # 'random_initialization',
     # 'gvsl_paper_released_weights',
+    # 'gvsl_original_nako1000_pretraining',
     # 'restoration_only(models_genesis_augmentations)',
     # 'registration_only_1000steps(small_synthetic_elastic_deformations)',
     # 'registration_only_50000steps(small_synthetic_elastic_deformations)',
@@ -36,7 +43,8 @@ plot_names = [
     # 'registration_restoration_50000steps',
     # 'registration_constrained_50000steps',
     # 'coupled_convex_50000steps',
-    'global_local_50000steps'
+    # 'global_local_50000steps',
+    'vox2vec_original'
 ]
 
 
@@ -63,9 +71,9 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Create data loader
 val_files = read_json_data_file(
-    data_file_path='/mnt/share/data/amos/dataset_preprocessed_mri/annotations/sup_train_24_f0.json',
-    data_dir='/mnt/share/data/amos/',
-    key='validation'
+    data_file_path='/mnt/share/data/nako_1000/annotations/niiregion2_democomplete_wmod_tr178.json',
+    data_dir='/mnt/share/data/nako_1000_affine/nii_region2_wcontrast_preprocessed/',
+    key='training'
 )
 
 val_ds = Dataset(data=val_files, transform=get_val_transform())
@@ -110,6 +118,15 @@ for data_idx, data in enumerate(val_loader):
             )
             state_dict = torch.load(weights)
             model.load_state_dict(state_dict['feature_extractor'])
+        elif 'vox2vec' in weights:
+            model = FPN3d(1, 16, 6)
+            state_dict = torch.load(weights)['state_dict']
+            for key in list(state_dict.keys()):
+                if 'backbone' not in key:
+                    del state_dict[key]
+            for key in list(state_dict.keys()):
+                state_dict[key.replace("backbone.", "")] = state_dict.pop(key)
+            model.load_state_dict(state_dict)
         else:
             model = UNet3D_GVSL(
                 n_channels=1,
@@ -120,11 +137,61 @@ for data_idx, data in enumerate(val_loader):
         model.eval()
 
         # Extract features
-        _, features = model(image)
-        features = features.detach().cpu().numpy()[0]
 
-        _, features_rot90 = model(image_2)
-        features_rot90 = features_rot90.detach().cpu().numpy()[0]
+        # features_1 = model(image)
+        # features_2 = model(image_2)
+        #
+        # global_registration_flow = coupled_convex(feat_fix=features_1[3], feat_mov=features_2[3], use_ice=False, img_shape=image.shape[2:])
+        # grid = functional.affine_grid(torch.eye(3, 4).unsqueeze(0).cuda(), (1, 1, image.shape[2], image.shape[3], image.shape[4]))
+        # image_2_warped = functional.grid_sample(image_2, grid + global_registration_flow.permute(0, 2, 3, 4, 1), mode='bilinear')
+        #
+        # image_original_np = image.data.cpu().numpy()[0, 0, ...].copy()
+        # center_slice = image_original_np.shape[2] // 2
+        # for image_slice in range(center_slice, center_slice + 1):
+        #     plt.imshow(image_original_np[:, :, image_slice], cmap='gray')
+        #     plt.colorbar()
+        #     plt.show()
+        #     plt.close()
+        #
+        # image_original_np = image_2.data.cpu().numpy()[0, 0, ...].copy()
+        # center_slice = image_original_np.shape[2] // 2
+        # for image_slice in range(center_slice, center_slice + 1):
+        #     plt.imshow(image_original_np[:, :, image_slice], cmap='gray')
+        #     plt.colorbar()
+        #     plt.show()
+        #     plt.close()
+        #
+        # image_original_np = image_2_warped.data.cpu().numpy()[0, 0, ...].copy()
+        # center_slice = image_original_np.shape[2] // 2
+        # for image_slice in range(center_slice, center_slice + 1):
+        #     plt.imshow(image_original_np[:, :, image_slice], cmap='gray')
+        #     plt.colorbar()
+        #     plt.show()
+        #     plt.close()
+
+        # _, features = model(image)
+        # features = features.detach().cpu().numpy()[0]
+
+        features = model(image)
+        features = [feature.detach().cpu() for feature in features]
+        feature_map = []
+        for feature in features:
+            feature_map.append(torch.nn.functional.interpolate(feature, size=(128, 128, 128), mode='trilinear'))
+        features = torch.cat(feature_map, dim=1)[0]
+        features = features.numpy()
+        print('Features of 1st image are prepared')
+
+        # _, features_rot90 = model(image_2)
+        # features_rot90 = features_rot90.detach().cpu().numpy()[0]
+
+        features_rot90 = model(image_2)
+        features_rot90 = [feature.detach().cpu() for feature in features_rot90]
+        feature_map_rot90 = []
+        for feature in features_rot90:
+            feature_map_rot90.append(torch.nn.functional.interpolate(feature, size=(128, 128, 128), mode='trilinear'))
+        features_rot90 = torch.cat(feature_map_rot90, dim=1)[0]
+        features_rot90 = features_rot90.numpy()
+        print('Features of 2nd image are prepared')
 
         features_list = []
         features_rot90_list = []
@@ -155,7 +222,7 @@ for data_idx, data in enumerate(val_loader):
                             labels_rot90_list[0], labels_rot90_list[1], labels_rot90_list[2], labels_rot90_list[5], labels_rot90_list[6]]),
             num_classes=10,  #len(labels),
             title=plot_name,
-            output_path=os.path.join(OUTPUT_DIR, f'{plot_name}_im{data_idx}_2images.png'),
+            # output_path=os.path.join(OUTPUT_DIR, f'{plot_name}_im{data_idx}_2images.png'),
             show=True)
 
         plot_tsne(
@@ -163,7 +230,7 @@ for data_idx, data in enumerate(val_loader):
             np.concatenate(labels_list),
             num_classes=len(labels),
             title=plot_name,
-            output_path=os.path.join(OUTPUT_DIR, f'{plot_name}_single_image_{data_idx}.png'),
+            # output_path=os.path.join(OUTPUT_DIR, f'{plot_name}_single_image_{data_idx}.png'),
             show=True)
 
 
